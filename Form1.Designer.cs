b54202f5425412b0a612e8a12e31fdf8955be534
Greede82 <egor.poleacov@gmail.com>
2025-06-18 17:50:05 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 446);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(157, 446);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Заменить  слово";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(291, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 84);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "найти полиномы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(468, 446);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 84);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "исправить текст";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(644, 445);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 84);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "посчитать колличество предложений";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(675, 559);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Колличество предложений";
+            this.label1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(826, 445);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 84);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "посчитать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1002, 445);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 85);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "кодировать";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1110, 445);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 83);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "декодировать";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(104, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1008, 328);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(891, 559);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(437, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Колличество предложений";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::lab4.Properties.Resources.water_turquoise_wave_azure_sea_fun_1562395_pxhere_com;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1216, 622);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Текстовое поле";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ApplyColorPalette(); 
+        }
+        public string main_text;
+        public string zamena1;
+        public string zamena2;
+    
+        private void ApplyColorPalette()
+        {
+
+            Color primaryColor = ColorTranslator.FromHtml("#217074");
+            Color secondaryColor = ColorTranslator.FromHtml("#37745B");
+            Color accentColor = ColorTranslator.FromHtml("#8B9D77");
+            Color backgroundColor = ColorTranslator.FromHtml("#E7EAEF");
+            Color buttonColor = ColorTranslator.FromHtml("#EDC5AB");
+
+            this.BackColor = backgroundColor; 
+
+            button1.BackColor = buttonColor;
+            button2.BackColor = buttonColor;
+            button3.BackColor = buttonColor;
+            button4.BackColor = buttonColor;
+            button5.BackColor = buttonColor;
+            button6.BackColor = buttonColor;
+            button7.BackColor = buttonColor;
+
+
+            button1.ForeColor = Color.Black;
+            button2.ForeColor = Color.Black;
+            button3.ForeColor = Color.Black;
+            button4.ForeColor = Color.Black;
+            button5.ForeColor = Color.Black;
+            button6.ForeColor = Color.Black;
+            button7.ForeColor = Color.Black;
+
+
+            richTextBox1.BackColor = secondaryColor;
+            richTextBox1.ForeColor = Color.White;
+
+            label1.ForeColor = primaryColor;
+            label2.ForeColor = primaryColor;
+
+
+            textBox2.BackColor = accentColor;
+            textBox3.BackColor = accentColor;
+            textBox2.ForeColor = Color.Black;
+            textBox3.ForeColor = Color.Black;
+        }
+
+
+        public static string Encode(string input)
+        {
+            StringBuilder encodedText = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                int asciiCode = (int)c;
+                int r = i % 3;
+                asciiCode += r;
+
+                encodedText.Append((char)asciiCode);
+            }
+
+            return encodedText.ToString();
+        }
+
+
+        public static string Decode(string input)
+        {
+            StringBuilder decodedText = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                int asciiCode = (int)c;
+                int r = i % 3;
+                asciiCode -= r;
+
+                decodedText.Append((char)asciiCode);
+            }
+
+            return decodedText.ToString();
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string inputText = richTextBox1.Text;
+
+            string encodedText = Encode(inputText);
+
+            richTextBox1.Text = encodedText;
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            string inputText = richTextBox1.Text;
+
+            string decodedText = Decode(inputText);
+
+            richTextBox1.Text = decodedText;
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            main_text = richTextBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            char[] main_textArray = main_text.ToCharArray();
+            for (int i = 0; i < main_textArray.Length - 2; i++)
+            {
+                if (main_textArray[i] == '.' && char.IsLower(main_textArray[i + 2]))
+                {
+                    main_textArray[i + 2] = char.ToUpper(main_textArray[i + 2]);
+                }
+                main_textArray[0] = char.ToUpper(main_textArray[0]);
+            }
+
+            main_text = new string(main_textArray);
+            richTextBox1.Text = main_text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            zamena1 = textBox2.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            zamena2 = textBox3.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pattern = zamena1;
+            string target = zamena2;
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(main_text, target);
+            richTextBox1.Text = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            for (var i = 0; i < main_text.Length; i++)
+            {
+                if (main_text[i] == '.' || main_text[i] == '!' || main_text[i] == '?')
+                {
+                    count++;
+                }
+            }
+            label1.Visible = true;
+            label1.Text = $"{count}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pattern = @"\b\w+\b";
+            Regex regex = new Regex(pattern);
+
+            int index = 0;
+
+            foreach (Match match in regex.Matches(main_text))
+            {
+                string word = match.Value;
+
+                if (IsPalindrome(word))
+                {
+                    richTextBox1.Select(match.Index, match.Length);
+                    richTextBox1.SelectionColor = Color.Red;
+                }
+            }
+        }
+
+        private bool IsPalindrome(string word)
+        {
+            string cleanedWord = word.ToLower();
+            char[] charArray = cleanedWord.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedWord = new string(charArray);
+            return cleanedWord == reversedWord;
+        }
+
+        private double EvaluateExpression(string expression)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                return Convert.ToDouble(table.Compute(expression, string.Empty));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректное арифметическое выражение.");
+                return 0;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string expression = richTextBox1.Text;
+            double result = EvaluateExpression(expression);
+            label2.Visible = true;
+            label2.Text = $"{result}";
+        }
+    }
+}
